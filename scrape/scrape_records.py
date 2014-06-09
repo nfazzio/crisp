6af2d7315e65e4a6fedea6d21ee0e089eab0c28f
= <nfazzio@gmail.com>
2014-06-09 15:39:29 -0500

    scrapes more records

--- scrape/scrape_records.py
+++ scrape/scrape_records.py
@@ -1,29 +1,44 @@
 import urllib2, os
+from bs4 import BeautifulSoup as bs
+import re
 
-url = 'http://gaceta.diputados.gob.mx/Gaceta/Iniciativas/62/gp62_a1primero.html'
+def main():
+    setup_dirs()
+    base_url = 'http://gaceta.diputados.gob.mx/Gaceta/Iniciativas/'
+    directory_soup = get_soup(base_url)
+    links = directory_soup.findAll('a')
+    for link in links:
+        download_iniciativa(link, base_url)
 
-directory = os.path.abspath('downloads')
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def setup_dirs():
+    if not os.path.exists('downloads/iniciativas'):
+        os.makedirs('downloads/iniciativas/')
 
-file_name = url.split('/')[-1]
-u = urllib2.urlopen(url)
-f = open(os.path.join('downloads',file_name), 'wb')
-meta = u.info()
-file_size = int(meta.getheaders("Content-Length")[0])
-print "Downloading: %s Bytes: %s" % (file_name, file_size)
+def get_soup(url):
+    base = urllib2.urlopen(url)
+    soup = bs(base.read())
+    base.close()
+    return soup
 
-file_size_dl = 0
-block_sz = 8192
-while True:
-    buffer = u.read(block_sz)
-    if not buffer:
-        break
+def download_iniciativa(link, base_url):
+    link = link['href']
+    if (link == '57/gp57_iniciativas.html') or ('mailto' in link):
+        print "skipping " + base_url+link
+        return
+    print base_url+link
+    iniciativas = urllib2.urlopen(base_url+link)
+    soup = bs(iniciativas)
+    iniciativas.close()
+    #filename = soup.find('font', {'size' : '-1'}).string.replace(' ','_')
+    print link
+    #filename = re.search('(?:\/)(.*)', link).group(1)
+    filename = os.path.basename(link)
+    print filename
+    iniciativa_html = os.path.join('downloads/iniciativas', filename)
+    with open(iniciativa_html, 'w+') as iniciativa:
+        iniciativa.write(str(soup))
 
-    file_size_dl += len(buffer)
-    f.write(buffer)
-    status = r"%10d [%3.2f%%]:" % (file_size_dl, file_size_dl * 100. / file_size)
-    status = status + chr(8)*(len(status)+1)
-    print status,
 
-f.close()
+
+if __name__ == "__main__":
+    main()
